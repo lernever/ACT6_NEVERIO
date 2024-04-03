6f424ef3fa84d64e6539ed55eb714fb2bf7cc059
lernever <neveriolerma07@gmail.com>
2024-04-03 20:59:26 +0800

    added index and show

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activity 6 Neverio</title>
+</head>
+<body>
+    <h1>My Activity 6</h1>
+    <ul>
+        <li><a href="/posts/1">Posts 1</a></li>
+        <li><a href="/posts/2">Posts 2</a></li>
+    </ul>
+</body>
+</html>
